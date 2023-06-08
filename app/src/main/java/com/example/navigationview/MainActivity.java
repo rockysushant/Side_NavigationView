500fe702920c27c8c72fd195ada3f277999f1fa3
rockysushant <sushantsoni018@gmail.com>
2023-06-08 17:46:50 +0530

    First Push

---
+++ app/src/main/java/com/example/navigationview/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.navigationview;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+
+    @SuppressLint({"WrongViewCast", "MissingInflatedId"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+    }
+
+    public void ClickMenu(View view){opeDrawer(drawerLayout);}
+
+    private void opeDrawer(DrawerLayout){
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+
+    public void dashboard(View view){
+        Intent intent = new Intent(MainActivity.this,MainActivity.class);
+        startActivity(intent);
+
+    }
+
+
+    public void settings(View view){
+        Intent intent = new Intent(MainActivity.this,Settings.class);
+        startActivity(intent);
+
+    }
+
+
+    public void logOut(View view){
+
+        logoutMenu(MainActivity.this);
+
+    }
+
+    private void logoutMenu(MainActivity mainActivity){
+        AlertDialog.Builder builder = new AlertDialog.Builder(mainActivity);
+        builder.setTitle("LogOut");
+        builder.setMessage("Are you sure to logout");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/navigationview/Settings.java
@@ -0,0 +1,28 @@
+package com.example.navigationview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+
+    }
+
+
+    public void back(View view){
+        Intent intent = new Intent(Settings.this,MainActivity.class);
+        startActivity(intent);
+
+    }
+
+
+
+}
